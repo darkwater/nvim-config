c65bede8cc32fb38ddec320de39dd0cf3e8bd021
darkwater <dark@dark.red>
2026-05-08 12:39:12 +0200

    add some keybinds

--- lua/dark-config/keybinds.lua
+++ lua/dark-config/keybinds.lua
@@ -2,13 +2,27 @@ local wk = require("which-key")
 local nv = { "n", "v" }
 local iv = { "i", "v" }
 
+local fn = require("dark-config.functions")
+local telescope = require("telescope.builtin")
+
+wk.setup {
+    preset = "helix",
+    icons = { mappings = false },
+    expand = 5,
+}
+
+-- NOTE: commas are replaced with \x2c to avoid Tabularize aligning on them
+
 wk.add {
     -- some basic keybinds
     { "<C-s>", "<Cmd>write<CR>",      desc = "Save file",           mode = nv },
     { "<C-s>", "<Esc><Cmd>write<CR>", desc = "Save file",           mode = iv },
     { "gg",    "gg0",                 desc = "Go to start of file", mode = nv },
     { "G",     "G0",                  desc = "Go to end of file",   mode = nv },
-    { "<cr>",  "<Cmd>e #<CR>",        desc = "Switch to alternate file" },
+    { "<CR>",  "<Cmd>e #<CR>",        desc = "Switch to alternate file" },
+
+    { "'",  fn.open_terminal,   desc = "Open external terminal" },
+    { "gd", fn.goto_definition, desc = "Go to definition", mode = nv },
 
     -- leader keybinds
     { "<leader><Tab>",     group = "Tabularize" },
@@ -23,12 +37,11 @@ wk.add {
     { "<leader><Tab>:",    ":Tabularize /:\\zs/l0r1<CR>",       desc = "foo: bar",           mode = nv },
     { "<leader><Tab>\x2c", ":Tabularize/\x2c\\zs\\ze/l0r1<CR>", desc = "foo\x2c bar",        mode = nv },
 
-    { "<leader>w", proxy = "<C-w>" },
+    { "<leader>w",  group = "window",      proxy = "<C-w>" },
+    { "<leader>wt", ":Neotree reveal<CR>", desc = "Open Neotree" },
 
-    { "'", function()
-        vim.system(
-            { "kitty", "--directory", vim.fn.getcwd() },
-            { detach = true }
-        )
-    end, desc = "Toggle terminal" },
+    { "<leader>p",     group = "pickers" },
+    { "<leader>pf",    telescope.find_files,                          desc = "Find files" },
+    { "<leader>ph",    fn.file_picker_in(fn.xdg_config_path("hypr")), desc = "Hyprland config" },
+    { "<leader>p\x2c", fn.file_picker_in(fn.xdg_config_path("nvim")), desc = "Neovim config" },
 }
