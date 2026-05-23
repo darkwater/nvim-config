514f1036dfc740d921b4b68ffeed859864bf3ca7
darkwater <dark@dark.red>
2026-05-23 22:25:48 +0200

    feat(plugins/snacks): move notifications to bottom-right corner

--- lua/dark-config/plugins/snacks.lua
+++ lua/dark-config/plugins/snacks.lua
@@ -2,6 +2,11 @@ require("snacks").setup {
     notifier = {
         enabled = true,
         timeout = 5000,
+        top_down = false,
+        margin = {
+            bottom = 2,
+            right = 2,
+        },
     },
     input = {
         enabled = true,
