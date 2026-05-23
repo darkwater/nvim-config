2b8c7ed8bb2362626dee42963d58d4cd12e02b5f
darkwater <dark@dark.red>
2026-05-23 22:07:43 +0200

    feat(plugins/neotree): change file icon

--- lua/dark-config/plugins/neotree.lua
+++ lua/dark-config/plugins/neotree.lua
@@ -6,4 +6,20 @@ require("neo-tree").setup {
             },
         },
     },
+    default_component_configs = {
+        icon = {
+            folder_closed = "",
+            folder_open = "",
+            folder_empty = "",
+            folder_empty_open = "",
+            default = "󰧮",
+            use_filtered_colors = true,
+
+            ---@diagnostic disable-next-line: unused-local
+            provider = function(config, node, state)
+                -- TODO: figure out how to detect symlinks and other special files
+                -- config.text = " "
+            end,
+        },
+    },
 }
